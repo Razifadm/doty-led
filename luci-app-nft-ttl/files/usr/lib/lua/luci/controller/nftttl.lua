5b41918da83ce5861da95a596fcf193ffc909bc7
Doty The Cat <dotywrt@gmail.com>
2025-08-04 15:16:28 +0800

    Update nftttl.lua

--- luci-app-nft-ttl/files/usr/lib/lua/luci/controller/nftttl.lua
+++ luci-app-nft-ttl/files/usr/lib/lua/luci/controller/nftttl.lua
@@ -2,5 +2,5 @@ module("luci.controller.nftttl", package.seeall)
 
 function index()
 	if not nixio.fs.access("/etc/config/nftttl") then return end
-	entry({"admin", "modem", "nftttl"}, cbi("nftttl"), _("TTL Settings"), 60).acl_depends = { "luci-app-nft-ttl" }
+	entry({"admin", "tools", "nftttl"}, cbi("nftttl"), _("TTL Settings"), 60).acl_depends = { "luci-app-nft-ttl" }
 end
