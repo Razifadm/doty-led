4a9a0ee52fdd6993d0f55d4d52848b0b92699541
Doty The Cat <dotywrt@gmail.com>
2025-08-04 15:43:24 +0800

    Update netstat.lua

--- luci-app-netstat/files/usr/lib/lua/luci/controller/netstat.lua
+++ luci-app-netstat/files/usr/lib/lua/luci/controller/netstat.lua
@@ -1,5 +1,6 @@
 module("luci.controller.netstat", package.seeall)
 
 function index()
+	entry({"admin", "tools"}, firstchild(), _("Tools"), 50).dependent = false
 	entry({"admin", "tools", "netstat_config"}, cbi("netstat/config"), _("Netstat Config"), 20).leaf = true
 end
