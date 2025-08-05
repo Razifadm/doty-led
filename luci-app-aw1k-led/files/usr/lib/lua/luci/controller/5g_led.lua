931b9d6f4aa1776294a4af90b309d38a85ab7d30
Doty The Cat <dotywrt@gmail.com>
2025-08-05 08:29:37 +0800

    Update 5g_led.lua

--- luci-app-aw1k-led/files/usr/lib/lua/luci/controller/5g_led.lua
+++ luci-app-aw1k-led/files/usr/lib/lua/luci/controller/5g_led.lua
@@ -1,10 +1,12 @@
 module("luci.controller.5g_led", package.seeall)
 
 function index()
+    entry({"admin", "tools"}, firstchild(), _("Tools"), 50).dependent = false
+
     entry({"admin", "tools", "5g"}, firstchild(), _("5G LED Config"), 60).dependent = false
 
-    entry({"admin", "tools", "5g", "excellent"}, cbi("5g/excellent"), _("Excellent"), 10)
-    entry({"admin", "tools", "5g", "good"}, cbi("5g/good"), _("Good"), 20)
-    entry({"admin", "tools", "5g", "average"}, cbi("5g/average"), _("Average"), 30)
-    entry({"admin", "tools", "5g", "bad"}, cbi("5g/bad"), _("Bad"), 40)
+    entry({"admin", "tools", "5g", "excellent"}, cbi("5g.excellent"), _("Excellent"), 10).leaf = true
+    entry({"admin", "tools", "5g", "good"}, cbi("5g.good"), _("Good"), 20).leaf = true
+    entry({"admin", "tools", "5g", "average"}, cbi("5g.average"), _("Average"), 30).leaf = true
+    entry({"admin", "tools", "5g", "bad"}, cbi("5g.bad"), _("Bad"), 40).leaf = true
 end
