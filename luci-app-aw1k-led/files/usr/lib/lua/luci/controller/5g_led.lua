b699f9c727abfd2fe16187110398140ad1195394
Doty The Cat <dotywrt@gmail.com>
2025-08-07 21:20:49 +0800

    Update 5g_led.lua

--- luci-app-aw1k-led/files/usr/lib/lua/luci/controller/5g_led.lua
+++ luci-app-aw1k-led/files/usr/lib/lua/luci/controller/5g_led.lua
@@ -6,9 +6,7 @@ function index()
     end
 
     entry({"admin", "tools", "5g"}, firstchild(), _("5G LED Config"), 60).dependent = false
-    entry({"admin", "tools", "5g", "excellent"}, cbi("5g/excellent"), _("Excellent"), 1)
-    entry({"admin", "tools", "5g", "good"}, cbi("5g/good"), _("Good"), 10)
-    entry({"admin", "tools", "5g", "average"}, cbi("5g/average"), _("Average"), 20)
-    entry({"admin", "tools", "5g", "bad"}, cbi("5g/bad"), _("Bad"), 30)
-    entry({"admin", "tools", "5g", "settings"}, cbi("5g/settings"), _("Settings"), 40)
+    entry({"admin", "tools", "5g", "quality"}, cbi("5g/quality"), _("Quality"), 1)
+    entry({"admin", "tools", "5g", "settings"}, cbi("5g/settings"), _("LED Settings"), 2)
+    entry({"admin", "tools", "5g", "status"}, cbi("5g/status"), _("Signal Status"), 3)
 end
